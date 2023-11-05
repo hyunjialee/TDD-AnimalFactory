645a49855d4932e27c1d9e1e8c2b2b6fe7a34b28
hyunjialee <hyjialee@gmail.com>
2023-11-05 17:55:57 -0500

    finished test cases for dog

--- src/test/java/rocks/zipcodewilmington/DogTest.java
+++ src/test/java/rocks/zipcodewilmington/DogTest.java
@@ -2,7 +2,11 @@ package rocks.zipcodewilmington;
 
 import org.junit.Assert;
 import org.junit.Test;
+import rocks.zipcodewilmington.animals.Animal;
 import rocks.zipcodewilmington.animals.Dog;
+import rocks.zipcodewilmington.animals.Mammal;
+
+import java.util.Date;
 
 /**
  * @author leon on 4/19/18.
@@ -28,4 +32,67 @@ public class DogTest {
         String dogName = dog.getName();
         Assert.assertEquals(dogName, givenName);
     }
+    @Test
+    public void testSpeak() {
+        Dog dog = new Dog (null, null,null);
+        String expected = "bark!";
+
+        String actual = dog.speak();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testBirth() {
+        Dog dog = new Dog (null, null, null);
+        Date expected = new Date();
+
+        dog.setBirthDate(expected);
+        Date actual = dog.getBirthDate();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFood(){
+        Dog dog = new Dog(null,null,null);
+        Food dogFood = new Food();
+
+        Integer food = 1;
+
+        dog.eat(dogFood);
+        Integer actual = dog.getNumberOfMealsEaten();
+
+        Assert.assertEquals(food, actual);
+    }
+    @Test
+    public void getDogID(){
+        Dog dog = new Dog(null, null, 123);
+        Integer expected = 123;
+
+        Integer actual = dog.getId();
+
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void getAnimalTest(){
+        Animal dog = new Dog(null, null,null);
+        Boolean expected = true;
+
+        Boolean actual = dog instanceof Animal;
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getMammalTest(){
+        Mammal dog = new Dog(null, null,null);
+        Boolean expected = true;
+
+        Boolean actual = dog instanceof Mammal;
+
+        Assert.assertEquals(expected, actual);
+    }
+
+
 }
